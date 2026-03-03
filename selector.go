29798eedda2a5819a79121b09bde5bc8064e2426
Dmytro <dimkanp1@gmail.com>
2026-03-03 21:17:19 +0200

    Rework Selector

--- selector.go
+++ selector.go
@@ -1,18 +1,50 @@
 package selector
 
-type Selector map[string]any
+import "fmt"
 
-func ToSelector(s any) Selector {
-	switch v := s.(type) {
-	case Selector:
-		return v
+type Selector struct {
+	Name      string
+	Params    map[string]any
+	Subfields []*Selector
+}
+
+func Select(input ...any) []*Selector {
+	var results []*Selector
+	for _, in := range input {
+		results = append(results, normalize(in)...)
+	}
+	return results
+}
+
+func normalize(input any) []*Selector {
+	switch in := input.(type) {
+	case *Selector:
+		return []*Selector{in}
+	case []*Selector:
+		return in
+	case string:
+		return []*Selector{{Name: in}}
+	case []string:
+		list := make([]*Selector, len(in))
+		for i, s := range in {
+			list[i] = &Selector{Name: s}
+		}
+		return list
 	default:
-		return BaseSelector()
+		panic(fmt.Sprintf("type %T not supported as Selector", input))
 	}
 }
 
-func BaseSelector() Selector {
-	return Selector{
-		"_system_base_fields": struct{}{},
+func DefaultSelector() *Selector {
+	return &Selector{Name: "default"}
+}
+
+func GetParameter[T any](s *Selector, name string) (res T, ok bool) {
+	p, ok := s.Params[name]
+	if !ok {
+		return res, false
 	}
+
+	res, ok = p.(T)
+	return res, ok
 }
