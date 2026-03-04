ba8ea759ba3cf3fec6396cb369dcfe76d9866ce6
Dmytro <dimkanp1@gmail.com>
2026-03-04 16:13:12 +0200

    Set and get methods for Selector.Alias

--- selector.go
+++ selector.go
@@ -59,3 +59,50 @@ func GetParameter[T any](s *Selector, name string) (res T, ok bool) {
 func (s *Selector) IsDefault() bool {
 	return s.Name == defaultName && len(s.Params) == 0 && len(s.Fields) == 0
 }
+
+// SetAlias fieldsTree parameter used to set Alias for nested structure.
+// Can be omitted to set Alias for current structure Selector or
+// passed few values for several levels of nesting
+func (s *Selector) SetAlias(alias string, fieldsTree ...string) error {
+	if len(fieldsTree) == 0 {
+		s.Alias = alias
+		return nil
+	}
+
+	name := fieldsTree[0]
+
+	for _, field := range s.Fields {
+		if field.Name == name {
+			err := field.SetAlias(alias, fieldsTree[1:]...)
+			if err != nil {
+				return fmt.Errorf("%s.%w", name, err)
+			}
+		}
+	}
+
+	return fmt.Errorf("%s field not found", name)
+}
+
+// GetAlias fieldsTree parameter used to get Alias of nested structure.
+// Can be omitted to get Alias of current structure Selector or
+// passed few values for several levels of nesting
+func (s *Selector) GetAlias(fieldsTree ...string) (string, error) {
+	if len(fieldsTree) == 0 {
+		return s.Alias, nil
+	}
+
+	name := fieldsTree[0]
+
+	for _, field := range s.Fields {
+		if field.Name == name {
+			alias, err := field.GetAlias(fieldsTree[1:]...)
+			if err != nil {
+				return "", fmt.Errorf("%s.%w", name, err)
+			}
+
+			return alias, nil
+		}
+	}
+
+	return "", fmt.Errorf("%s field not found", name)
+}
